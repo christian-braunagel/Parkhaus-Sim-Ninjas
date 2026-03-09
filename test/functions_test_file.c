ffa9630e8e4c09b6b864219d88c406804643815e
Nikolai-Slsr <163042351+Nikolai-Slsr@users.noreply.github.com>
2026-03-09 17:47:35 +0100

    Finished unit tests for queue.c

--- test/functions_test_file.c
+++ test/functions_test_file.c
@@ -11,17 +11,41 @@ ausprinten.*/
 #include <stdlib.h>
 #include <assert.h>
 
-
+/**
+ * @brief Unit tests for the functions in queue.c
+ * 
+ * This function tests the following functions:
+ * - init_queue
+ * - enqueue
+ * - dequeue
+ * - free_queue
+ * 
+ */
 void queue_test(){
     //init_queue test
     queue *my_queue = init_queue();
     assert(my_queue != NULL);
     
     //enqueue test
-    assert(enqueue(my_queue, 1, 5, 0) == 0);
-    my_queue =NULL;                             //reset the queue for the next test
-    assert(enqueue(my_queue, 2, 3, 1) == -1);   //test for invalid queue pointer
+    assert(enqueue(my_queue, 1, 5, 1) == 0);
+    assert(enqueue(my_queue, -100, 5, -1) == -1);   //test invalid input parameters, should return -1
+    my_queue = NULL;                                 //reset the queue for the next test
+    assert(enqueue(my_queue, 2, 3, 3) == -1);       //test for invalid queue pointer
 
+    //dequeue test
+    assert(dequeue(my_queue) == NULL);              //test for NULL queue pointer, should return NULL
+    my_queue = init_queue();
+    assert(dequeue(my_queue) == NULL);              //test for empty queue, should return NULL
+    enqueue(my_queue, 1, 5, 1);
+    assert(dequeue(my_queue)->vehicle_id == 1);     //test for correct dequeue
+
+    //print_queue test will not be tested here because it only prints the queue and does not return any value, so we would have to check the output manually, which is not ideal for automated testing. (Was manualy tested and works as expected)
+
+    //test freeing the queue
+    assert(free_queue(&my_queue) == 0);             //test for successful freeing
+    assert(my_queue == NULL);                       //check if the original queue pointer is set to NULL
+    my_queue = NULL;
+    assert(free_queue(&my_queue) == -1);            //test for invalid queue pointer, should return -1
 }
 
 void parkhaus_test(){
